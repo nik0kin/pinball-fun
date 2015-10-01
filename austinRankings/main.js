e1d99871c84ea7c9ee5753c711feaefb0c22cf5a
Nikolas Poklitar <nikolas.poklitar@gmail.com>
2015-10-01 10:05:37 -0500

    a little more style

--- austinRankings/main.js
+++ austinRankings/main.js
@@ -2,13 +2,13 @@
 $(document).ready(function () {
   _.each(PINBALL_PLAYERS, function (playerInfo, i) {
     playerInfo.austinRank = i + 1;
-    var template = '<div class="row">'
-      +  '<div class="col-md-1"><%= austinRank %></div>'
-      +  '<div class="col-md-1"><%= texasRank %></div>'
-      +  '<div class="col-md-1"><%= worldRank %></div>'
-      +  '<div class="col-md-3"><%= playerName %></div>'
-      +  '<div class="col-md-1"><%= points %></div>'
-      +  '<div class="col-md-1"><%= events %></div>'
+    var template = '<div class="row ' + (i % 2 === 1 ? 'active' : '') + '">'
+      +  '<div class="col-xs-1"><b><%= austinRank %></b></div>'
+      +  '<div class="col-xs-1"><%= texasRank %></div>'
+      +  '<div class="col-xs-1"><%= worldRank %></div>'
+      +  '<div class="col-xs-3"><%= playerName %></div>'
+      +  '<div class="col-xs-1"><%= points %></div>'
+      +  '<div class="col-xs-1"><%= events %></div>'
       +'</>';
     var htmlRow = _.template(template)(playerInfo);
     $('.container').append(htmlRow);
